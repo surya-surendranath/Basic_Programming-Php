f4828c38fa4e7a48ca6bb90a6700aa6da1029792
surya-surendranath <surya.snath08@gmail.com>
2016-08-25 13:15:01 +1200

    create myDB database

--- php/mysql.php
+++ php/mysql.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
+$username = "root";
+$password = "sumesh";
 
 $conn = new mysqli($servername, $username, $password);
 
@@ -9,4 +9,14 @@ if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected succesfully";
+
+$sql = "CREATE DATABASE myDB";
+	if ($conn->query($sql) === TRUE) {
+		echo "Database created succesfully";
+	}
+	else {
+		echo "Error creating database: " . $conn->error;
+	}
+
+	$conn->close();
 ?>
