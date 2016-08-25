e0a15dd77a6d400069d8a76848abadecfc39c71e
surya-surendranath <surya.snath08@gmail.com>
2016-08-25 13:27:37 +1200

    create myEmployee table created

--- php/mysql.php
+++ php/mysql.php
@@ -2,20 +2,29 @@
 $servername = "localhost";
 $username = "root";
 $password = "sumesh";
+$dbname = "myDB";
 
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected succesfully";
 
-$sql = "CREATE DATABASE myDB";
-	if ($conn->query($sql) === TRUE) {
-		echo "Database created succesfully";
-	}
-	else {
-		echo "Error creating database: " . $conn->error;
+
+$sql = "CREATE TABLE MYemployee (
+	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	firstname VARCHAR(30) NOT NULL,
+	lastname VARCHAR(30) NOT NULL,
+	email VARCHAR(50),
+	reg_date TIMESTAMP 
+	)";
+
+	if ($conn->query($sql) ===TRUE) {
+		echo "Table MYemployee created succesfully";
+
+	} else {
+		echo "Error creating table : " . $conn->error;
 	}
 
 	$conn->close();
