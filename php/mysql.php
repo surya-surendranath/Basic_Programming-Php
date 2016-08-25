a814f6f431f61c14131f8c1a641e0fac4b64efb4
surya-surendranath <surya.snath08@gmail.com>
2016-08-25 13:58:44 +1200

    inserted data to Myemployee

--- php/mysql.php
+++ php/mysql.php
@@ -11,17 +11,29 @@ if ($conn->connect_error) {
 }
 echo "Connected succesfully";
 
+/*$sql = "CREATE DATABASE myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+  */
 
-$sql = "CREATE TABLE MYemployee (
+/*$sql = "CREATE TABLE MYemployee (
 	id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	firstname VARCHAR(30) NOT NULL,
 	lastname VARCHAR(30) NOT NULL,
 	email VARCHAR(50),
 	reg_date TIMESTAMP 
 	)";
+	*/
+
+$sql = "INSERT INTO MYemployee (firstname, lastname, email) VALUES ('neo', 'jenny', 'jenny@gmail.com')";
 
 	if ($conn->query($sql) ===TRUE) {
-		echo "Table MYemployee created succesfully";
+		$last_id =$conn->insert_id;
+		//echo "Table MYemployee created succesfully";
+		echo "New record created succesfully, Last inserted id is", $last_id ;
 
 	} else {
 		echo "Error creating table : " . $conn->error;
